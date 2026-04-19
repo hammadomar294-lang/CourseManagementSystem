41853b4fe07e47f21d4b2f73992b25efada734f5
Omar Magdy <hammadomar294@gmail.com>
2026-04-19 22:21:45 +0200

    added basmala functions

--- source/basmala.cpp
+++ source/basmala.cpp
@@ -1,11 +1,48 @@
 // TODO: show all courses in the system
 void ViewAllCourses()
 {
+if (CourseCounter==0) {
+    cout << "No courses found in the system." << endl;
+    AskForString("Press any key to go back...");
+    return;
+}
+
+cout << "list of all courses" << endl;
+for(int i=0;i<CourseCounter;i++){
+
+    cout<<"CourseId: "<< 
+     CourseArray[i].CourseId << "\t"
+    <<"| Name: "<<
+     CourseArray[i].Name << "\t"
+    <<"| Instructor: "<<
+     CourseArray[i].InstructorName << "\t"
+    <<"| Capacity: "<<
+     CourseArray[i].CurrentEnrolled << "\t"
+     <<"/"<<
+     CourseArray[i].MaxCapacity << "\t"
+     <<" | Day: "<<
+     CourseArray[i].Day << "\t"
+     <<" | Time: "<<
+     CourseArray[i].Time << "\t" <<endl;
+}
+AskForString("Press any key to go back...");
 }
 
 // TODO: display grades for the current student about a specific course
 void ShowGrade()
 {
+string course=AskForString("Enter course name:");
+
+int cId = FindCourseIdByName(course);
+int index = FindStudentCourseIndexById ( CurrentStudentId , cId);
+
+if(index !=-1){
+    cout<<"Grade: "<<StudentCourseArray[index].Grade<<endl;
+}
+else{
+    cout<<"Course not found or not registered!"<<endl;
+}
+AskForString("Pressany key to go back...");
 }
 
 
--- source/main.cpp
+++ source/main.cpp
@@ -577,8 +577,34 @@ void ViewAllStudents()
 
 
 // TODO: show all courses in the system
+// Done by basmala
 void ViewAllCourses()
 {
+if (CourseCounter==0) {
+    cout << "No courses found in the system." << endl;
+    AskForString("Press any key to go back...");
+    return;
+}
+
+cout << "list of all courses" << endl;
+for(int i=0;i<CourseCounter;i++){
+
+    cout<<"CourseId: "<< 
+     CourseArray[i].CourseId << "\t"
+    <<"| Name: "<<
+     CourseArray[i].Name << "\t"
+    <<"| Instructor: "<<
+     CourseArray[i].InstructorName << "\t"
+    <<"| Capacity: "<<
+     CourseArray[i].CurrentEnrolled << "\t"
+     <<"/"<<
+     CourseArray[i].MaxCapacity << "\t"
+     <<" | Day: "<<
+     CourseArray[i].Day << "\t"
+     <<" | Time: "<<
+     CourseArray[i].Time << "\t" <<endl;
+}
+AskForString("Press any key to go back...");
 }
 
 // TODO: show all courses a specific student is registered in
@@ -810,8 +836,21 @@ void ViewMyCourses()
 }
 
 // TODO: show grade for the student's course 
-void ViewGrade()
+// Done by basmala
+void ShowGrade()
 {
+string course=AskForString("Enter course name:");
+
+int cId = FindCourseIdByName(course);
+int index = FindStudentCourseIndexById ( CurrentStudentId , cId);
+
+if(index !=-1){
+    cout<<"Grade: "<<StudentCourseArray[index].Grade<<endl;
+}
+else{
+    cout<<"Course not found or not registered!"<<endl;
+}
+AskForString("Press any key to go back...");
 }
 #pragma endregion
 
